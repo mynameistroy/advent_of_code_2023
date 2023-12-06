2dd3f27cb0e00df4681f37632cf8b60f12378ace
jokker <jokker@gmail.com>
2023-12-06 16:14:15 -0500

    3-2

--- third/src/main.rs
+++ third/src/main.rs
@@ -1,10 +1,12 @@
 mod input;
+use core::num;
+
 use crate::input::{EXAMPLE_INPUT, PUZZLE_INPUT, ANOTHER_EXAMPLE};
 
 fn part_one() {
     let mut schematic: Vec<Vec<char>> = Vec::new();
 
-    for line in PUZZLE_INPUT.lines() {
+    for line in EXAMPLE_INPUT.lines() {
         schematic.push(line.chars().collect());
     }
 
@@ -75,8 +77,8 @@ fn part_one() {
                         //println!("{part} {is_part}");
                         // if it preceded or succeeded the number on the same line
                         if is_part {
-                            for _c in part_number.chars() {
-                                print!("_");
+                            for c in part_number.chars() {
+                                print!("{c}");
                             }
                             part_numbers.push(part);
                         } else {
@@ -95,8 +97,8 @@ fn part_one() {
             
             // if it preceded or succeeded the number on the same line
             if is_part {
-                for _c in part_number.chars() {
-                    print!("_");
+                for c in part_number.chars() {
+                    print!("{c}");
                 }
                 part_numbers.push(part);
             } else {
@@ -111,8 +113,149 @@ fn part_one() {
     println!("Value: {part_numbers_sum}");
 }
 
-fn part_two() {
+fn check_line_left(line: &[char], point: usize) -> Option<usize> {
+    let mut number = String::default();
+    for c in line[..point].into_iter().rev() {
+        if c.is_ascii_digit() {
+            number.insert(0, *c);
+        } else {
+            break;
+        }
+    }
+    if !number.is_empty() {
+        return Some(number.parse().unwrap());
+    } else {
+        None
+    }
+}
 
+fn check_line_right(line: &[char], point: usize) -> Option<usize> {
+    let mut number = String::default();
+    for c in line[point + 1..].into_iter() {
+        if c.is_ascii_digit() {
+            number.push(*c);
+        } else {
+            break;
+        }
+    }
+    if !number.is_empty() {
+        return Some(number.parse().unwrap());
+    } else {
+        None
+    }
+}
+
+fn check_directly(line: &[char], point: usize) -> Option<usize> {
+    let mut number = String::from(line[point]);
+    for c in line[..point].into_iter().rev() {
+        if c.is_ascii_digit() {
+            number.insert(0, *c);
+        } else {
+            break;
+        }
+    }
+    for c in line[point + 1..].into_iter() {
+        if c.is_ascii_digit() {
+            number.push(*c);
+        } else {
+            break;
+        }
+    }
+    if !number.is_empty() {
+        return Some(number.parse().unwrap());
+    } else {
+        None
+    }
+}
+
+fn part_two() {
+    let mut schematic: Vec<Vec<char>> = Vec::new();
+
+    for line in PUZZLE_INPUT.lines() {
+        schematic.push(line.chars().collect());
+    }
+
+    let mut gears: Vec<usize> = Vec::new();
+    let mut line_number = 0;
+    for line in &schematic {
+        let mut point_number = 0;
+        for point in line {
+            // is it a gear?
+            if *point == '*' {
+                let mut part_numbers: Vec<usize> = Vec::new();
+
+                // check above
+                if line_number > 0 {
+                    let line_above = &schematic[line_number - 1];
+                    // is the space above a number
+                    if !line_above[point_number].is_ascii_digit() {
+                        if point_number > 0 && line_above[point_number - 1].is_ascii_digit() {
+                            // there is a number to left
+                            if let Some(part) = check_line_left(&line_above, point_number) {
+                                part_numbers.push(part);
+                            }
+                        }
+                        if point_number < line.len() - 1 && line_above[point_number + 1].is_ascii_digit() {
+                            // there is a number to right
+                            if let Some(part) = check_line_right(&line_above, point_number) {
+                                part_numbers.push(part);
+                            }
+                        }
+                    } else {
+                        if let Some(part) = check_directly(&line_above, point_number) {
+                            part_numbers.push(part);
+                        }
+                    }
+                }
+
+                // check below
+                if line_number != schematic.len() - 1 {
+                    let line_below = &schematic[line_number + 1];
+                    // is the space above a number
+                    if !line_below[point_number].is_ascii_digit() {
+                        if point_number > 0 && line_below[point_number - 1].is_ascii_digit() {
+                            // there is a number to left
+                            if let Some(part) = check_line_left(&line_below, point_number) {
+                                part_numbers.push(part);
+                            }
+                        }
+                        if point_number < line.len() - 1 && line_below[point_number + 1].is_ascii_digit() {
+                            // there is a number to right
+                            if let Some(part) = check_line_right(&line_below, point_number) {
+                                part_numbers.push(part);
+                            }
+                        }
+                    } else {
+                        if let Some(part) = check_directly(&line_below, point_number) {
+                            part_numbers.push(part);
+                        }
+                    }
+                }
+
+                // check left side
+                if point_number > 0 {
+                    if let Some(part) = check_line_left(&line, point_number) {
+                        part_numbers.push(part);
+                    }
+                }
+
+                // check right side
+                if point_number < line.len() - 1 {
+                    if let Some(part) = check_line_right(&line, point_number) {
+                        part_numbers.push(part);
+                    }
+                }
+
+                if part_numbers.len() == 2 {
+                    // it's a real gear, get the ratio
+                    gears.push(part_numbers[0] * part_numbers[1]);
+                }
+            }
+            point_number += 1;
+        }
+        line_number += 1;
+    }
+    println!("Value: {}", gears.iter().sum::<usize>());
 }
 
 fn main() {
